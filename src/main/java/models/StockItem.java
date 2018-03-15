c07e8ace4747eda496b4ea2ebf8e13a3444dff15
Jim Bevington <jim.bevington@hotmail.com>
2018-03-15 12:10:57 +0000

    annotated StockItem

--- src/main/java/models/StockItem.java
+++ src/main/java/models/StockItem.java
@@ -2,7 +2,13 @@ package models;
 
 import behaviours.ISell;
 
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class StockItem implements ISell {
+
+    private int id;
     protected int buyPrice;
     protected int sellPrice;
 
@@ -11,11 +17,32 @@ public abstract class StockItem implements ISell {
         this.sellPrice = sellPrice;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="buyPrice")
     public int getBuyPrice() {
         return this.buyPrice;
     }
 
+    public void setBuyPrice(int buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    @Column(name="sellPrice")
     public int getSellPrice() {
         return this.sellPrice;
+    }
+
+    public void setSellPrice(int sellPrice) {
+        this.sellPrice = sellPrice;
     }
 }
