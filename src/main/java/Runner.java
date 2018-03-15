56da697ab135f168dc812a437b56cb782e0f36ea
Jim Bevington <jim.bevington@hotmail.com>
2018-03-15 12:32:14 +0000

    added getList method to DBHelper

---
+++ src/main/java/Runner.java
@@ -0,0 +1,36 @@
+import db.DBHelper;
+import models.Guitar;
+import models.MusicBook;
+import models.MusicStand;
+import models.Piano;
+
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Guitar guitar1 = new Guitar("Jazzman", "black", 6, 500, 1000);
+        DBHelper.saveOrUpdate(guitar1);
+        Guitar guitar2 = new Guitar("Speedster", "blue", 12, 800, 1600);
+        DBHelper.saveOrUpdate(guitar2);
+
+        Piano piano1 = new Piano("Steinway", "white", 500, 1000);
+        DBHelper.saveOrUpdate(piano1);
+        Piano piano2 = new Piano("Kawai", "coral pink", 500, 2000);
+        DBHelper.saveOrUpdate(piano2);
+
+        MusicBook book1 = new MusicBook("Songs for Shouting", 5, 10);
+        DBHelper.saveOrUpdate(book1);
+        MusicBook book2 = new MusicBook("Achingly Awful Airs", 4, 10);
+        DBHelper.saveOrUpdate(book2);
+
+        MusicStand stand1 = new MusicStand("white", 5, 10);
+        DBHelper.saveOrUpdate(stand1);
+        MusicStand stand2 = new MusicStand("ostrich feather black", 4, 8);
+        DBHelper.saveOrUpdate(stand2);
+
+        List<Guitar> guitars = DBHelper.getAll(Guitar.class);
+
+    }
+}
---
+++ src/main/java/db/DBHelper.java
@@ -0,0 +1,47 @@
+package db;
+
+import models.Guitar;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class DBHelper {
+
+    private static Transaction transaction;
+    private static Session session;
+
+    public static void saveOrUpdate(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(object);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+//    public static List<Guitar> getAll(Class<Guitar> guitarClass) {
+//    }
+
+    public static <T> List<T> getList(Criteria cr){
+        List<T> results = null;
+        try {
+            transaction = session.beginTransaction();
+            results = cr.list();
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+}
